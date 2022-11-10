fdb1634eb8dc1408c0ca82ba27c4c11e9d03e51c
unknown <vitobassily@gmail.com>
2022-11-10 21:21:14 +0200

    use beatifulsoup function

--- webSCR.py
+++ webSCR.py
@@ -14,3 +14,7 @@ result = requests.get("https://wuzzuf.net/search/jobs/?q=python+developer&a=hpb"
 src = result.content
 soup = BeautifulSoup( src, "lxml")
 
+job_title = soup.find_all("h2",{"class":"css-m604qf"})
+company_names = soup.find_all("a",{"class":"css-17s97q8"})
+descriptions = soup.find_all("div",{"class":"css-y4udm8"})
+locations = soup.find_all("span",{"class":"css-5wys0k"})
